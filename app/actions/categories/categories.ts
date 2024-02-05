312033016bb3cd24e2fb1669a8990bda116d2f23
ItsMaazMalick <maaz93707@gmail.com>
2024-02-05 17:57:38 +0500

    Update Category Route and add Category Action

---
+++ app/actions/categories/categories.ts
@@ -0,0 +1,44 @@
+import prisma from "@/lib/db";
+
+export async function getDataByCategory(category: string) {
+  if (category === "all") {
+    const data = await prisma.category.findMany({
+      select: {
+        id: true,
+        name: true,
+        slug: true,
+        items: {
+          select: {
+            id: true,
+            name: true,
+            images: true,
+            inStock: true,
+            originalPrice: true,
+            salePrice: true,
+          },
+        },
+      },
+    });
+    return data;
+  } else {
+    const data = await prisma.category.findMany({
+      where: { slug: category },
+      select: {
+        id: true,
+        name: true,
+        slug: true,
+        items: {
+          select: {
+            id: true,
+            name: true,
+            images: true,
+            inStock: true,
+            originalPrice: true,
+            salePrice: true,
+          },
+        },
+      },
+    });
+    return data;
+  }
+}
--- app/api/v1/[category]/route.ts
+++ app/api/v1/[category]/route.ts
@@ -1,6 +1,5 @@
-import { getPublicToken, verifyPublicToken } from "@/app/actions/publicToken";
-import { getAllFoods } from "@/constants/data";
-import prisma from "@/lib/db";
+import { getDataByCategory } from "@/app/actions/categories/categories";
+import { verifyPublicToken } from "@/app/actions/publicToken";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -12,49 +11,12 @@ export async function GET(request: Request) {
     }
     const { pathname } = new URL(request.url);
     const params = pathname.split("/v1/")[1];
-    if (params === "all") {
-      const data = await prisma.category.findMany({
-        select: {
-          id: true,
-          name: true,
-          slug: true,
-          items: {
-            select: {
-              id: true,
-              name: true,
-              images: true,
-              inStock: true,
-              originalPrice: true,
-              salePrice: true,
-            },
-          },
-        },
-      });
-      return NextResponse.json({ status: 200, data });
-    }
-    const data = await prisma.category.findMany({
-      where: { slug: params },
-      select: {
-        id: true,
-        name: true,
-        slug: true,
-        items: {
-          select: {
-            id: true,
-            name: true,
-            images: true,
-            inStock: true,
-            originalPrice: true,
-            salePrice: true,
-          },
-        },
-      },
-    });
-    if (data.length === 0) {
+    const data = await getDataByCategory(params);
+
+    if (!data || data.length === 0) {
       return NextResponse.json({ status: 200, message: "Invalid category" });
     }
 
-    console.log(params);
     return NextResponse.json({ status: 200, data });
   } catch (error) {
     console.log(error);
