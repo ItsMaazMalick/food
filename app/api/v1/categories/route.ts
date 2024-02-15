1de7538f029d629652ecf996efc41f06c3f250d1
ItsMaazMalick <maaz93707@gmail.com>
2024-02-15 19:27:09 +0500

    add force dynamic

--- app/api/v1/categories/route.ts
+++ app/api/v1/categories/route.ts
@@ -2,16 +2,18 @@ import { verifyPublicToken } from "@/app/actions/publicToken";
 import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
-    // const token = await verifyPublicToken();
-    // if (!token) {
-    //   return NextResponse.json({
-    //     status: 401,
-    //     success: false,
-    //     message: "Invalid request",
-    //   });
-    // }
+    const token = await verifyPublicToken();
+    if (!token) {
+      return NextResponse.json({
+        status: 401,
+        success: false,
+        message: "Invalid request",
+      });
+    }
     const data = await prisma.category.findMany();
     return NextResponse.json({ status: 200, success: true, data });
   } catch (error) {
