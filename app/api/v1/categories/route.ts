a1e250c93ba581d043dfd79683a1ea05e4040c38
ItsMaazMalick <maaz93707@gmail.com>
2024-02-15 16:35:48 +0500

    categories changes

--- app/api/v1/categories/route.ts
+++ app/api/v1/categories/route.ts
@@ -4,14 +4,15 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   try {
-    const token = request.headers.get("Authorization")?.split(" ")[1];
-    // const newToken = token?.replace("bearer ", "");
-    if (!token || !verifyPublicToken(token)) {
-      return NextResponse.json({ status: 401, message: "Invalid Request" });
-    }
+    // const token = request.headers.get("Authorization")?.split(" ")[1];
+    // // const newToken = token?.replace("bearer ", "");
+    // if (!token || !verifyPublicToken(token)) {
+    //   return NextResponse.json({ status: 401, message: "Invalid Request" });
+    // }
     const data = await prisma.category.findMany();
     return NextResponse.json({ status: 200, success: true, data });
   } catch (error) {
+    console.log(error);
     return NextResponse.json({
       status: 500,
       success: false,
