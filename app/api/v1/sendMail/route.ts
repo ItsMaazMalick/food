5dde6cd6b54bb88c03ce2cc7e32e14f0110e1e46
ItsMaazMalick <maaz93707@gmail.com>
2024-02-20 21:58:11 +0500

    Add header token in sendMail/route.ts

--- app/api/v1/sendMail/route.ts
+++ app/api/v1/sendMail/route.ts
@@ -1,8 +1,20 @@
+import { verifyPublicToken } from "@/app/actions/publicToken";
 import { sendOTP } from "@/utils/sendOTP";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(request: NextRequest) {
   try {
+    const token = await verifyPublicToken();
+    if (!token) {
+      return NextResponse.json({
+        status: 401,
+        success: false,
+        message: "Invalid request",
+      });
+    }
+
     const { name, email } = await request.json();
     if (!name || !email) {
       return NextResponse.json({
