f25a9474c9c2a138ebefffc44b47cb57a61dd844
ItsMaazMalick <maaz93707@gmail.com>
2024-02-25 13:34:14 +0500

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,45 +1,12 @@
 "use server";
 import { NextRequest, NextResponse } from "next/server";
 import { decryptString } from "./utils/encryption";
-import jwt from "jsonwebtoken";
-import { cookies } from "next/headers";
-import { deleteCookie } from "./app/actions/deleteCookie";
-
-export const dynamic = "force-dynamic";
 
 export async function middleware(request: NextRequest) {
-  async function redirectToLogin() {
-    await deleteCookie();
-    return NextResponse.redirect(new URL("/admin/auth/login", request.url));
-  }
-
-  const token = cookies().get("auth-token")?.value;
-  const userId = cookies().get("user")?.value;
-
+  const token = request.cookies.get("auth-token")?.value;
+  const userId = request.cookies.get("user")?.value;
   if (!token || !userId || !token.length) {
-    return redirectToLogin();
-  }
-
-  const decryptToken = await decryptString(token);
-  const decryptId = await decryptString(userId);
-  if (!decryptToken) {
-    return redirectToLogin();
-  }
-  const decodedToken = jwt.decode(decryptToken);
-  if (!decodedToken) {
-    return redirectToLogin();
-  }
-  const { tokenData }: any = decodedToken;
-  if (!tokenData) {
-    return redirectToLogin();
-  }
-  const id = tokenData.id;
-  if (!id) {
-    return redirectToLogin();
-  }
-
-  if (id !== decryptId) {
-    return redirectToLogin();
+    return NextResponse.redirect(new URL("/admin/auth/login", request.url));
   }
 
   return NextResponse.next();
