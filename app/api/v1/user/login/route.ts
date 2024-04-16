bc34ed63fe64c35a8317be6acb8c9798cc2000b9
ItsMaazMalick <maaz93707@gmail.com>
2024-04-16 19:18:19 +0500

    fixed: linkAccount with google

--- app/api/v1/user/login/route.ts
+++ app/api/v1/user/login/route.ts
@@ -4,6 +4,7 @@ import { decryptString, encryptString } from "@/utils/encryption";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { codeGenerator } from "@/lib/codeGenerator";
 
 export async function POST(request: Request) {
   try {
@@ -16,6 +17,8 @@ export async function POST(request: Request) {
       });
     }
     const hashPassword = await bcrypt.hash(password, 10);
+    const refCode = codeGenerator(16);
+
     if (!linkAccount || linkAccount === "credentials") {
       const res = await loginUser({ email, password });
       return NextResponse.json(res);
@@ -24,27 +27,21 @@ export async function POST(request: Request) {
       const existingUser = await prisma.user.findUnique({
         where: { email },
       });
-      if (!existingUser || existingUser.linkAccount !== "google") {
-        return NextResponse.json({
-          status: 401,
-          success: false,
-          // errors: {},
-          message: "Invalid credentials",
-        });
-      }
-      if (existingUser.linkAccount === "google") {
-        await prisma.user.update({
-          where: { email: existingUser.email },
+      // There is not existing user exist!
+      if (!existingUser) {
+        const user = await prisma.user.create({
           data: {
             name,
+            email,
             image,
             password: hashPassword,
+            referralCode: refCode,
+            linkAccount: "google",
           },
         });
-        //TOKEN DATA
         const tokenData = {
-          id: existingUser.id,
-          email: existingUser.email,
+          id: user.id,
+          email: user.email,
         };
 
         //ASSIGN TOKEN
@@ -59,14 +56,62 @@ export async function POST(request: Request) {
         return NextResponse.json({
           status: 200,
           success: true,
-          id: existingUser.id,
-          name: existingUser.name,
-          email: existingUser.email,
-          referralCode: existingUser.referralCode,
-          points: existingUser.points,
-          favorites: existingUser.favorites,
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          referralCode: user.referralCode,
+          points: user.points,
+          favorites: user.favorites,
           // errors: {},
           token: encryptToken,
+        });
+      }
+
+      // There is an existing user exist! but not link with google
+      if (existingUser && existingUser.linkAccount !== "google") {
+        return NextResponse.json({
+          status: 401,
+          success: false,
+          // errors: {},
+          message: "Invalid credentials",
+        });
+      }
+      // There is an existing user exist! also link with google
+      if (existingUser.linkAccount === "google") {
+        const user = await prisma.user.update({
+          where: { email: existingUser.email },
+          data: {
+            name,
+            image,
+            password: hashPassword,
+          },
+        });
+        //TOKEN DATA
+        const tokenData = {
+          id: user.id,
+          email: user.email,
+        };
+
+        //ASSIGN TOKEN
+        const token = jwt.sign(
+          {
+            tokenData,
+          },
+          process.env.JWT_SECRET!,
+          { expiresIn: "1d" }
+        );
+        const encryptToken = encryptString(token);
+        return NextResponse.json({
+          status: 200,
+          success: true,
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          referralCode: user.referralCode,
+          points: user.points,
+          favorites: user.favorites,
+          // errors: {},
+          token: user,
         });
       }
     }
